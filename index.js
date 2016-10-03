e1433641b39c99c56699245a2c84b43bd7f30c56
kakashisama <yrgodbole@gmail.com>
2016-10-02 21:23:03 -0400

    Changes to be committed: 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.sendFile('index.html');
 });
 
 app.listen(3000, function () {
