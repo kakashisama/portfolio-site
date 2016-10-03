036b2f95742e7780b8ba90c41f88ea3b332c1ee0
kakashisama <yrgodbole@gmail.com>
2016-10-02 21:15:33 -0400

    adding express and removing grunt-cli , gulp-cli

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /*
 My index.js file
 */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Yogis portfolio site app listening on port 3000!');
+});
